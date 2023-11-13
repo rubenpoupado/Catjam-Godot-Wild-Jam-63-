d2ab1d52f0fcd99fcca8865cb54c3a53348911e6
Rúben David Costa Poupado <aluno20966@ipt.pt>
2023-11-13 03:01:25 +0000

    Player Wall Parkour

--- Scripts/CatPlayer.gd
+++ Scripts/CatPlayer.gd
@@ -1,14 +1,13 @@
 extends CharacterBody2D
 
 
-const SPEED = 120.0
+const SPEED = 200.0
 const JUMP_VELOCITY = -320.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-var maxjumpCooldown = 1
-var jumpCooldown = 0
+var isParkouring = false
+var isSliding = false
 
 @onready var animationTree : AnimationTree = $AnimationTree
 
@@ -19,43 +18,52 @@ func _process(delta):
 	update_animation_parameters()
 	
 func _physics_process(delta):
-	jumpCooldown += delta
 	var direction = Input.get_axis("MoveLeft", "MoveRight")
-	if direction:
+	if direction and not isParkouring:
 		velocity.x = direction * SPEED
 		if direction < 0:
 			$CatSprite.flip_h = true
 		else:
 			$CatSprite.flip_h = false
-	else:
+	elif not isParkouring:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 			
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
-	if is_on_floor():
-		if Input.is_action_just_pressed("Jump") and jumpCooldown >= maxjumpCooldown:
-			jumpCooldown = 0
+		if is_on_wall():
+			isSliding = true
+		if isSliding and Input.is_action_just_pressed("Jump"):
+			$CatSprite.flip_h = not $CatSprite.flip_h
+			velocity.y = JUMP_VELOCITY * 1.5
+			if $CatSprite.flip_h:
+				velocity.x = -SPEED * 1.5
+			else:
+				velocity.x = SPEED * 1.5
+			isParkouring = true
+	else:
+		isParkouring = false
+		isSliding = false
+		if Input.is_action_just_pressed("Jump"):
 			velocity.y = JUMP_VELOCITY
+			
 	move_and_slide()
-
 func update_animation_parameters():
 	if velocity == Vector2.ZERO:
 		animationTree["parameters/conditions/isIdle"] = true
 		animationTree["parameters/conditions/isRunning"] = false
 		animationTree["parameters/conditions/isJumping"] = false
 		animationTree["parameters/conditions/isFalling"] = false
-	if velocity.x and is_on_floor():
+	elif velocity.x and is_on_floor():
 		animationTree["parameters/conditions/isIdle"] = false
 		animationTree["parameters/conditions/isRunning"] = true
 		animationTree["parameters/conditions/isJumping"] = false
 		animationTree["parameters/conditions/isFalling"] = false
-	if velocity.y < 0:
+	elif velocity.y < 0:
 		animationTree["parameters/conditions/isIdle"] = false
 		animationTree["parameters/conditions/isRunning"] = false
 		animationTree["parameters/conditions/isJumping"] = true
 		animationTree["parameters/conditions/isFalling"] = false
-	if velocity.y > 0:
+	elif velocity.y > 0:
 		animationTree["parameters/conditions/isIdle"] = false
 		animationTree["parameters/conditions/isRunning"] = false
 		animationTree["parameters/conditions/isJumping"] = false
