412b5ccd5d22651b54e900e601cafcae2cc0a62a
António Neves <aluno23321@ipt.pt>
2023-11-14 20:26:51 +0000

    moving platform and saw

---
+++ Scripts/saw_movement.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+const WAIT_DURATION = 1.0
+
+@onready var object = $saw as Area2D
+@export var speed = 5.0
+@export var distance = 64*5
+
+var follow = Vector2.ZERO
+var platform_center = 32
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	move_object()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	object.position = object.position.lerp(follow, 0.5)
+	
+func move_object():
+	var direction = Vector2.RIGHT * distance 
+	var duration = direction.length()/float(speed*platform_center)
+	
+	var platform_tween = create_tween().set_loops()
+	platform_tween.tween_property(self, "follow", direction, duration).set_delay(WAIT_DURATION)
+	platform_tween.tween_property(self, "follow", Vector2.ZERO, duration).set_delay(WAIT_DURATION)
--- Scripts/saw_rotation.gd
+++ Scripts/saw_rotation.gd
@@ -1,5 +1,6 @@
-extends Area2D
+extends Sprite2D
 
+@export var speed = 10.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,11 +9,4 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
-
-func _on_body_entered(body):
-	print("Body entered")
-	if body.name == "CatPlayer":
-		print("Player entered")
-		body.die()
-	pass # Replace with function body.
+	rotate(-delta*speed)
